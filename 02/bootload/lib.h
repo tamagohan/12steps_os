39e1763017fecbc715cdecfc74d9c8440f3dd6d4
tamagohan <fantastipo23@gmail.com>
2019-11-09 18:21:17 +0900

    Imp step2

--- 02/bootload/lib.h
+++ 02/bootload/lib.h
@@ -1,7 +1,15 @@
 #ifndef _LIB_H_INCLUDED_
 #define _LIB_H_INCLUDED_
 
-int putc(unsigned char c);    /* １文字送信 */
-int puts(unsigned char *str); /* 文字列送信 */
+void *memset(void *b, int c, long len);
+void *memcpy(void *dst, const void *src, long len);
+int memcpm(const void *b1, const void *b2, long len);
+int strlen(const char *s);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int len);
+
+int putc(unsigned char c);
+int puts(unsigned char *str);
+int putxval(unsigned long value, int column);
 
 #endif
--- 02/bootload/main.c
+++ 02/bootload/main.c
@@ -6,7 +6,10 @@ int main(void)
 {
   serial_init(SERIAL_DEFAULT_DEVICE);
 
-  puts("Hello World!\n");
+  puts("Hello World\n");
+
+  putxval(0x10, 0); puts("\n");
+  putxval(0xffff, 0); puts("\n");
 
   while (1)
     ;
